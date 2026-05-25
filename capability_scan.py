e5290c66a30310ce13a7ad0a8671d2d6acd79970
Amrit <as5318287@gmail.com>
2026-05-25 12:44:47 +0530

    Update capability_scan.py

--- capability_scan.py
+++ capability_scan.py
@@ -3,17 +3,31 @@ import subprocess
 def scan_capabilities():
     print("\n[+] SCANNING LINUX CAPABILITIES\n")
 
-    try:
-        output = subprocess.check_output(
-            "getcap -r / 2>/dev/null",
-            shell=True,
-            text=True
-        )
+    directories = [
+        "/usr/bin",
+        "/usr/sbin",
+        "/bin",
+        "/sbin"
+    ]
 
-        if output:
-            print(output)
-        else:
-            print("No capabilities found")
+    found = False
 
-    except Exception as e:
-        print(f"Error: {e}")
+    for directory in directories:
+        try:
+            print(f"[+] Checking: {directory}\n")
+
+            output = subprocess.check_output(
+                f"getcap -r {directory} 2>/dev/null",
+                shell=True,
+                text=True
+            )
+
+            if output.strip():
+                print(output)
+                found = True
+
+        except Exception:
+            pass
+
+    if not found:
+        print("No capabilities found")
